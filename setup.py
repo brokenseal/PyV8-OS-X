7ee1259bd80701177c8edd593e0dbb3321f10caf
Davide Callegari <admin@brokenseal.it>
2012-04-13 11:58:37 +0200

    Added setup.py file and customized it

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name = "PyV8",
+    version = "0.8",
+    author = "Callegari Davide",
+    author_email = "admin@brokenseal.it",
+    description = "Taken from http://www.dcl.hpi.uni-potsdam.de/home/loewis/pyv8/",
+    long_description = open("README").read(),
+    license = "",
+    url = "https://github.com/brokenseal/PyV8-OS-X",
+    packages=find_packages(),
+    #install_requires= [], # I wish I could use pip better
+    classifiers = [
+        "Development Status :: 0.8",
+        "Environment :: Server",
+        "Intended Audience :: Developers",
+        "License :: All rights reserved",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ]
+)
